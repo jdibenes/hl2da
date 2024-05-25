1429f385fc3a6def310706074ebd79b7e2b834e6
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-05-24 23:35:57 -0400

    plugin import

---
+++ hl2da/plugin/hl2da.h
@@ -0,0 +1,67 @@
+﻿
+#pragma once
+
+#include <stdint.h>
+
+#define PLUGIN_IMPORT extern "C" __declspec(dllimport)
+
+enum SensorId
+{
+    RM_VLC_LEFTFRONT,
+    RM_VLC_LEFTLEFT,
+    RM_VLC_RIGHTFRONT,
+    RM_VLC_RIGHTRIGHT,
+    RM_DEPTH_AHAT,
+    RM_DEPTH_LONGTHROW,
+    RM_IMU_ACCELEROMETER,
+    RM_IMU_GYROSCOPE,
+    RM_IMU_MAGNETOMETER
+};
+
+PLUGIN_IMPORT
+void DebugMessage(char const* str);
+
+PLUGIN_IMPORT
+void InitializeGlobal();
+
+PLUGIN_IMPORT
+void Initialize(int id, int buffer_size);
+
+PLUGIN_IMPORT
+void SetEnable(int id, int enable);
+
+PLUGIN_IMPORT
+int GetByFramestamp(int id, int32_t stamp, void** frame, uint64_t* timestamp, int32_t* framestamp);
+
+PLUGIN_IMPORT
+int GetByTimestamp(int id, uint64_t stamp, int time_preference, int tiebreak_right, void** frame, uint64_t* timestamp, int32_t* framestamp);
+
+PLUGIN_IMPORT
+void Release_RM(void* frame);
+
+PLUGIN_IMPORT
+void Extract_RM_VLC(void* frame, void const** buffer, int32_t* length, void** pose_buffer, int32_t* pose_length);
+
+PLUGIN_IMPORT
+void Extract_RM_Depth_AHAT(void* frame, void const** buffer, int32_t* length, void const** ab_depth_buffer, int32_t* ab_depth_length, void** pose_buffer, int32_t* pose_length);
+
+PLUGIN_IMPORT
+void Extract_RM_Depth_Longthrow(void* frame, void const** buffer, int32_t* length, void const** ab_depth_buffer, int32_t* ab_depth_length, void const** sigma_buffer, int32_t* sigma_length, void** pose_buffer, int32_t* pose_length);
+
+PLUGIN_IMPORT
+void Extract_RM_IMU_Accelerometer(void* frame, void const** buffer, int32_t* length, void** pose_buffer, int32_t* pose_length);
+
+PLUGIN_IMPORT
+void Extract_RM_IMU_Gyroscope(void* frame, void const** buffer, int32_t* length, void** pose_buffer, int32_t* pose_length);
+
+PLUGIN_IMPORT
+void Extract_RM_IMU_Magnetometer(void* frame, void const** buffer, int32_t* length, void** pose_buffer, int32_t* pose_length);
+
+PLUGIN_IMPORT
+void GetExtrinsics_RM(int id, float* out);
+
+PLUGIN_IMPORT
+void MapImagePointToCameraUnitPlane_RM(int id, float const* in, float* out, int point_count);
+
+PLUGIN_IMPORT
+void MapCameraSpaceToImagePoint_RM(int id, float const* in, float* out, int point_count);
