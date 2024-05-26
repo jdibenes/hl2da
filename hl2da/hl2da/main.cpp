52e83edfa1b5925c69bc7fc0362b5f870717281e
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-05-25 23:27:55 -0400

    pv test

--- hl2da/hl2da/main.cpp
+++ hl2da/hl2da/main.cpp
@@ -1,8 +1,10 @@
 
 #include <windows.h>
 #include "research_mode.h"
+#include "personal_video.h"
 #include "locator.h"
 #include "stream_rm.h"
+#include "stream_pv.h"
 #include "log.h"
 
 #include <winrt/Windows.Foundation.h>
@@ -58,6 +60,12 @@ struct App : winrt::implements<App, IFrameworkViewSource, IFrameworkView>
 
         if (m_init) { return; }
 
+        Locator_Initialize();
+        ResearchMode_Initialize();
+        PersonalVideo_Initialize();
+
+        RM_InitializeDepthLock();
+
         m_init = true;
     }
 
@@ -67,9 +75,32 @@ struct App : winrt::implements<App, IFrameworkViewSource, IFrameworkView>
 
         window.Activate();
 
+        pv_captureformat cf;
+        pv_frame* f;
+        uint64_t t;
+        int32_t s;
+        pv_data d;
+        cf.mrcvo.enable = false;
+        cf.mrcvo.shared = false;
+        cf.vf.width = 640;
+        cf.vf.height = 360;
+        cf.vf.framerate = 30;
+
+        PV_Initialize(10);
+        PV_SetFormat(cf);
+        PV_SetEnable(true);
+
         while (!m_windowClosed)
 		{
 		window.Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+
+        int v = PV_Get(-1, f, t, s);
+        if (v == 0)
+        {
+            PV_Extract(f->mfr, d);
+            ShowMessage("GOT PV FRAME %lld %p %d", t, d.buffer, d.length);
+            f->Release();
+        }
 		}
 
 		CoreApplication::Exit();
