cc6f92029ef024e9b35a706269f1d31565869f3d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-05-30 13:27:37 -0400

    group plugin

--- hl2da/plugin/hl2da.h
+++ hl2da/plugin/hl2da.h
@@ -2,11 +2,10 @@
 #pragma once
 
 #include <stdint.h>
-#include "researchmode/ResearchModeApi.h"
 
 #define PLUGIN_IMPORT extern "C" __declspec(dllimport)
 
-enum sensor_id
+enum class SENSOR_ID : int32_t
 {
     RM_VLC_LEFTFRONT,
     RM_VLC_LEFTLEFT,
@@ -44,40 +43,10 @@ struct MagDataStruct
     uint64_t VinylHupTicks;
     uint64_t SocTicks;
     float MagValues[3];
+    float _reserved;
 };
 
-struct pv_videoformat
-{
-    uint16_t width;
-    uint16_t height;
-    uint8_t framerate;
-};
-
-struct mrc_video_options
-{
-    bool enable;
-    bool hologram_composition;
-    bool recording_indicator;
-    bool video_stabilization;
-    bool blank_protected;
-    bool show_mesh;
-    bool shared;
-    uint8_t _reserved[1];
-    float global_opacity;
-    float output_width;
-    float output_height;
-    uint32_t video_stabilization_length;
-    uint32_t hologram_perspective;
-};
-
-struct pv_captureformat
-{
-    mrc_video_options mrcvo;
-    pv_videoformat vf;
-    uint8_t _reserved[2];
-};
-
-struct joint_pose
+struct JointPose
 {
     float rx;
     float ry;
@@ -88,6 +57,79 @@ struct joint_pose
     float tz;
     float radius;
     int32_t accuracy;
+};
+
+enum class STATUS : int32_t
+{
+    DISCARDED = -1,
+    OK = 0,
+    WAIT = 1,
+};
+
+enum class TIME_PREFERENCE : int32_t
+{
+    PAST = -1,
+    NEAREST = 0,
+    FUTURE = 1,
+};
+
+enum class MC_CHANNELS : int32_t
+{
+    USE_2 = 0,
+    USE_5 = 1,
+};
+
+enum class EE_FPS_INDEX : int32_t
+{
+    FPS_30 = 0,
+    FPS_60 = 1,
+    FPS_90 = 2,
+};
+
+enum SI_VALID : int32_t
+{
+    VALID_HEAD = 1,
+    VALID_EYE = 2,
+    VALID_LEFT = 4,
+    VALID_RIGHT = 8,
+};
+
+enum EE_VALID : int32_t
+{
+    VALID_CALIBRATION = 1,
+    VALID_COMBINED_GAZE = 2,
+    VALID_LEFT_GAZE = 4,
+    VALID_RIGHT_GAZE = 8,
+    VALID_LEFT_OPENNESS = 16,
+    VALID_RIGHT_OPENNESS = 32,
+    VALID_VERGENCE_DISTANCE = 64,
+};
+
+enum class HOLOGRAM_PERSPECTIVE : int32_t
+{
+    DISPLAY = 0,
+    PV = 1,
+};
+
+struct pv_captureformat
+{
+    bool enable_mrc;
+    bool hologram_composition;
+    bool recording_indicator;
+    bool video_stabilization;
+    bool blank_protected;
+    bool show_mesh;
+    bool shared;
+    uint8_t _reserved_0[1];
+    float global_opacity;
+    float output_width;
+    float output_height;
+    uint32_t video_stabilization_length;
+    uint32_t hologram_perspective;
+    uint16_t width;
+    uint16_t height;
+    uint8_t framerate;
+    uint8_t _reserved_1[3];
 };
 
 PLUGIN_IMPORT
@@ -133,7 +175,7 @@ PLUGIN_IMPORT
 void MapCameraSpaceToImagePoint_RM(int id, float const* in, float* out, int point_count);
 
 PLUGIN_IMPORT
-void SetFormat_PV(pv_captureformat const* cf);
+void SetFormat_PV(void const* cf);
 
 PLUGIN_IMPORT
 void SetFormat_MC(int raw);
