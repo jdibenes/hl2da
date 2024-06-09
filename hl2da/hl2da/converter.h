3c9d264b34c413ce14232a6d13f8e3a513675a4d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-06-09 16:19:40 -0400

    Update converter.h

--- hl2da/hl2da/converter.h
+++ hl2da/hl2da/converter.h
@@ -7,7 +7,7 @@ void Converter_Initialize();
 void Converter_Cleanup();
 
 void Converter_ZHTInvalidate(uint16_t const* depth_in, uint16_t* depth_out);
-void Converter_ZLTInvalidate(uint8_t* const sigma_in, uint16_t const* depth_in, uint16_t* depth_out);
-void Converter_YUV2RGB(uint8_t* const image, uint32_t stride, uint32_t height, uint32_t format_in, uint32_t format_out, void** fc);
+void Converter_ZLTInvalidate(uint8_t const* sigma_in, uint16_t const* depth_in, uint16_t* depth_out);
+void Converter_YUV2RGB(uint8_t* image, uint32_t stride, uint32_t height, uint32_t format_in, uint32_t format_out, void** fc);
 void Converter_Extract(void* fc, void const** buffer, int32_t* length);
 void Converter_Release(void* fc);
