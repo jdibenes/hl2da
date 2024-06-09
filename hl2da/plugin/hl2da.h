c4c8422e3dc510f41416c904079e344d48103115
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-06-09 16:19:46 -0400

    Update hl2da.h

--- hl2da/plugin/hl2da.h
+++ hl2da/plugin/hl2da.h
@@ -441,10 +441,10 @@ PLUGIN_IMPORT
 void IMT_ZHTInvalidate(uint16_t const* depth_in, uint16_t* depth_out);
 
 PLUGIN_IMPORT
-void IMT_ZLTInvalidate(uint8_t* const sigma_in, uint16_t const* depth_in, uint16_t* depth_out);
+void IMT_ZLTInvalidate(uint8_t const* sigma_in, uint16_t const* depth_in, uint16_t* depth_out);
 
 PLUGIN_IMPORT
-void IMT_YUV2RGB(uint8_t* const image, uint32_t stride, uint32_t height, uint32_t format_in, uint32_t format_out, void** fc);
+void IMT_YUV2RGB(uint8_t* image, uint32_t stride, uint32_t height, uint32_t format_in, uint32_t format_out, void** fc);
 
 PLUGIN_IMPORT
 void IMT_Extract(void* fc, void const** buffer, int32_t* length);
