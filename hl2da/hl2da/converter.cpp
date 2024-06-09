8968b024a1be493588ab4b8b67b2437c254dd3fe
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-06-09 16:19:24 -0400

    Update converter.cpp

--- hl2da/hl2da/converter.cpp
+++ hl2da/hl2da/converter.cpp
@@ -260,13 +260,13 @@ void Converter_ZHTInvalidate(uint16_t const* depth_in, uint16_t* depth_out)
 }
 
 // OK
-void Converter_ZLTInvalidate(uint8_t* const sigma_in, uint16_t const* depth_in, uint16_t* depth_out)
+void Converter_ZLTInvalidate(uint8_t const* sigma_in, uint16_t const* depth_in, uint16_t* depth_out)
 {
     Neon_ZLTInvalidate(sigma_in, depth_in, depth_out);
 }
 
 // OK
-void Converter_YUV2RGB(uint8_t* const image, uint32_t stride, uint32_t height, uint32_t format_in, uint32_t format_out, void** fc)
+void Converter_YUV2RGB(uint8_t* image, uint32_t stride, uint32_t height, uint32_t format_in, uint32_t format_out, void** fc)
 {
     uint32_t const pixels = stride * height;
 
