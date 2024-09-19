80fb82c99e15e1610a769c14095755020b94a9ca
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-09-19 03:28:41 -0400

    Update stream_pv.cpp

--- hl2da/hl2da/stream_pv.cpp
+++ hl2da/hl2da/stream_pv.cpp
@@ -1,7 +1,9 @@
 
+#include <mfapi.h>
 #include "stream_pv.h"
 #include "personal_video.h"
 #include "locator.h"
+#include "extended_execution.h"
 #include "frame_buffer.h"
 #include "timestamps.h"
 #include "log.h"
@@ -18,7 +20,32 @@ using namespace winrt::Windows::Media::Capture;
 using namespace winrt::Windows::Media::Capture::Frames;
 using namespace winrt::Windows::Media::Devices::Core;
 using namespace winrt::Windows::Foundation::Numerics;
+using namespace winrt::Windows::Foundation::Collections;
 using namespace winrt::Windows::Perception::Spatial;
+
+struct uint64x2
+{
+    uint64_t val[2];
+};
+
+struct float7
+{
+    float val[7];
+};
+
+struct pv_metadata
+{
+    winrt::Windows::Foundation::Numerics::float4 intrinsics;
+    uint64_t exposure_time;
+    uint64x2 exposure_compensation;
+    uint32_t lens_position;
+    uint32_t focus_state;
+    uint32_t iso_speed;
+    uint32_t white_balance;
+    float2   iso_gains;
+    float3   white_balance_gains;
+    float7   extrinsics_mf;
+};
 
 class pv_frame : public sensor_frame
 {
@@ -28,10 +55,10 @@ public:
     winrt::Windows::Foundation::IMemoryBufferReference ref;
 
     winrt::Windows::Media::Capture::Frames::MediaFrameReference mfr;
-    winrt::Windows::Foundation::Numerics::float4 intrinsics;
+    pv_metadata pv;
     winrt::Windows::Foundation::Numerics::float4x4 pose;
 
-    pv_frame(winrt::Windows::Media::Capture::Frames::MediaFrameReference const& f, winrt::Windows::Foundation::Numerics::float4 const& k, winrt::Windows::Foundation::Numerics::float4x4 const& p);
+    pv_frame(winrt::Windows::Media::Capture::Frames::MediaFrameReference const& f, pv_metadata const& m, winrt::Windows::Foundation::Numerics::float4x4 const& p);
     ~pv_frame();
 };
 
@@ -54,7 +81,7 @@ static MRCVideoOptions g_options;
 //-----------------------------------------------------------------------------
 
 // OK
-pv_frame::pv_frame(MediaFrameReference const& f, float4 const& k, float4x4 const& p) : sensor_frame(), mfr(f), intrinsics(k), pose(p)
+pv_frame::pv_frame(MediaFrameReference const& f, pv_metadata const& m, float4x4 const& p) : sensor_frame(), mfr(f), pv(m), pose(p)
 {
     bmp = f.VideoMediaFrame().SoftwareBitmap();
     buf = bmp.LockBuffer(winrt::Windows::Graphics::Imaging::BitmapBufferAccessMode::Read);
@@ -80,8 +107,8 @@ static void PV_OnVideoFrameArrived(MediaFrameReader const& sender, MediaFrameArr
     int64_t timestamp;
     float2 f;
     float2 c;
-    float4 k;
     float4x4 pose;
+    pv_metadata m;
 
     if (!g_reader_status) { return; }
     frame = sender.TryAcquireLatestFrame();
@@ -90,25 +117,41 @@ static void PV_OnVideoFrameArrived(MediaFrameReader const& sender, MediaFrameArr
     timestamp = frame.SystemRelativeTime().Value().count();
 
     intrinsics = frame.VideoMediaFrame().CameraIntrinsics();
+    
     f = intrinsics.FocalLength();
     c = intrinsics.PrincipalPoint();
-    k = float4(f.x, f.y, c.x, c.y);
+
+    auto const& metadata = frame.Properties().Lookup(MFSampleExtension_CaptureMetadata).as<IMapView<winrt::guid, winrt::Windows::Foundation::IInspectable>>();
+    auto const& extrinsics = frame.Properties().Lookup(MFSampleExtension_CameraExtrinsics).as<winrt::Windows::Foundation::IReferenceArray<uint8_t>>().Value();
+
+    m.intrinsics = float4(f.x, f.y, c.x, c.y);
+    m.exposure_time = metadata.Lookup(MF_CAPTURE_METADATA_EXPOSURE_TIME).as<uint64_t>();
+    m.exposure_compensation = *(uint64x2*)metadata.Lookup(MF_CAPTURE_METADATA_EXPOSURE_COMPENSATION).as<winrt::Windows::Foundation::IReferenceArray<uint8_t>>().Value().begin();
+    m.iso_speed = metadata.Lookup(MF_CAPTURE_METADATA_ISO_SPEED).as<uint32_t>();
+    m.iso_gains = *(float2*)metadata.Lookup(MF_CAPTURE_METADATA_ISO_GAINS).as<winrt::Windows::Foundation::IReferenceArray<uint8_t>>().Value().begin();
+    m.lens_position = metadata.Lookup(MF_CAPTURE_METADATA_LENS_POSITION).as<uint32_t>();
+    m.focus_state = metadata.Lookup(MF_CAPTURE_METADATA_FOCUSSTATE).as<uint32_t>();
+    m.white_balance = metadata.Lookup(MF_CAPTURE_METADATA_WHITEBALANCE).as<uint32_t>();
+    m.white_balance_gains = *(float3*)metadata.Lookup(MF_CAPTURE_METADATA_WHITEBALANCE_GAINS).as<winrt::Windows::Foundation::IReferenceArray<uint8_t>>().Value().begin();
+    m.extrinsics_mf = *(float7*)&((float*)extrinsics.begin())[5];
 
     pose = Locator_GetTransformTo(frame.CoordinateSystem(), Locator_GetWorldCoordinateSystem(QPCTimestampToPerceptionTimestamp(timestamp)));
 
-    g_buffer.Insert(new pv_frame(frame, k, pose), timestamp);
+    g_buffer.Insert(new pv_frame(frame, m, pose), timestamp);
 
     if (WaitForSingleObject(g_event_enable, 0) != WAIT_OBJECT_0) { SetEvent(g_event_client); }
 }
 
 // OK
-static void PV_Acquire()
+static void PV_Acquire(int base_priority)
 {
     MediaFrameReader videoFrameReader = nullptr;
     bool ok;
 
     WaitForSingleObject(g_event_enable, INFINITE);
     WaitForSingleObject(g_event_client, 0);
+
+    SetThreadPriority(GetCurrentThread(), ExtendedExecution_GetInterfacePriority(INTERFACE_ID::ID_PV));
 
     PersonalVideo_Open(g_options);
 
@@ -131,9 +174,9 @@ static void PV_Acquire()
     g_buffer.Clear();
     }
 
-    PersonalVideo_Close();
+    SetThreadPriority(GetCurrentThread(), base_priority);
 
-    ResetEvent(g_event_enable);
+    while (WaitForSingleObject(g_event_enable, 0) == WAIT_OBJECT_0) { Sleep(1); }
 }
 
 // OK
@@ -141,7 +184,8 @@ static DWORD WINAPI PV_EntryPoint(void *param)
 {
     (void)param;
     PersonalVideo_RegisterEvent(g_event_client);
-    do { PV_Acquire(); } while (WaitForSingleObject(g_event_quit, 0) == WAIT_TIMEOUT);
+    int base_priority = GetThreadPriority(GetCurrentThread());
+    do { PV_Acquire(base_priority); } while (WaitForSingleObject(g_event_quit, 0) == WAIT_TIMEOUT);
     return 0;
 }
 
@@ -183,8 +227,8 @@ void PV_Extract(void* frame, void const** buffer, int32_t* length, void const** 
 
     *buffer = f->ref.data();
     *length = (int32_t)f->ref.Capacity();
-    *intrinsics_buffer = &f->intrinsics;
-    *intrinsics_length = sizeof(pv_frame::intrinsics) / sizeof(float);
+    *intrinsics_buffer = &f->pv;
+    *intrinsics_length = sizeof(pv_frame::pv) / sizeof(pv_metadata);
     *pose_buffer = &f->pose;
     *pose_length = sizeof(pv_frame::pose) / sizeof(float);
 }
