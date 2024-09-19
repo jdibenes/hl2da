0a635af3a7c3930a04e610f2fc0f5f66517a95cb
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-09-19 01:47:26 -0400

    Update extended_execution.h

--- hl2da/hl2da/extended_execution.h
+++ hl2da/hl2da/extended_execution.h
@@ -3,6 +3,25 @@
 
 #include <stdint.h>
 
+enum INTERFACE_ID
+{
+    ID_RM_VLF =  0,
+    ID_RM_VLL =  1,
+    ID_RM_VRF =  2,
+    ID_RM_VRR =  3,
+    ID_RM_ZHT =  4,
+    ID_RM_ZLT =  5,
+    ID_RM_ACC =  6,
+    ID_RM_GYR =  7,
+    ID_RM_MAG =  8,
+    ID_PV     = 10,
+    ID_MC     = 11,
+    ID_SI     = 12,
+    ID_EET    = 17,
+    ID_EA     = 18,
+    ID_EV     = 19,
+};
+
 void ExtendedExecution_Request();
 bool ExtendedExecution_Status();
 void ExtendedExecution_SetInterfacePriority(uint32_t id, int32_t priority);
