e6d7510e48a446d3fa37fc003a1d9c1c1f2ab728
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-09-19 03:28:46 -0400

    Update stream_rm.h

--- hl2da/hl2da/stream_rm.h
+++ hl2da/hl2da/stream_rm.h
@@ -10,7 +10,7 @@ int RM_Get(int id, uint64_t timestamp, int time_preference, bool tiebreak_right,
 
 void RM_Release(void* frame);
 
-void RM_Extract_VLC(void* frame, void const** buffer, int32_t* length, void const** pose_buffer, int32_t* pose_length);
+void RM_Extract_VLC(void* frame, void const** buffer, int32_t* length, void const** metadata_buffer, int32_t* metadata_length, void const** pose_buffer, int32_t* pose_length);
 void RM_Extract_Depth_AHAT(void* frame, void const** buffer, int32_t* length, void const** ab_depth_buffer, int32_t* ab_depth_length, void const** pose_buffer, int32_t* pose_length);
 void RM_Extract_Depth_Longthrow(void* frame, void const** buffer, int32_t* length, void const** ab_depth_buffer, int32_t* ab_depth_length, void const** sigma_buffer, int32_t* sigma_length, void const** pose_buffer, int32_t* pose_length);
 void RM_Extract_IMU_Accelerometer(void* frame, void const** buffer, int32_t* length, void const** pose_buffer, int32_t* pose_length);
@@ -24,6 +24,7 @@ void RM_MapCameraSpaceToImagePoint(int id, float const* in, float* out, int poin
 void RM_InitializeDepthLock();
 void RM_CleanupDepthLock();
 void RM_BypassDepthLock(bool bypass);
+void RM_VLC_SetConstantFactor(int64_t factor);
 
 void RM_Initialize(int id, int32_t buffer_size);
 void RM_Quit(int id);
