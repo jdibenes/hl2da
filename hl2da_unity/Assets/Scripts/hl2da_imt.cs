b0649c09a4e69afcb6710b2c3264232b6a4fcc56
jdibenes <58836755+jdibenes@users.noreply.github.com>
2024-09-22 18:53:45 -0400

    Update hl2da_imt.cs

--- hl2da_unity/Assets/Scripts/hl2da_imt.cs
+++ hl2da_unity/Assets/Scripts/hl2da_imt.cs
@@ -1,51 +1,60 @@
 
 using System;
 
-public class hl2da_imt
+public static partial class hl2da_api
 {
-    private IntPtr container;
-
-    private IntPtr buffer;
-    private int length;
-
-    public hl2da_imt()
+    public class imt
     {
-        Reset();
-    }
+        private IntPtr container;
 
-    ~hl2da_imt()
-    {
-        Destroy();
-    }
-    
-    public IntPtr Buffer { get { return buffer; } }
+        private IntPtr buffer;
+        private int length;
 
-    public int Length { get { return length; } }
-    
-    private void Reset()
-    {
-        container = IntPtr.Zero;
-        buffer = IntPtr.Zero;
-        length = 0;
-    }
+        public imt()
+        {
+            Reset();
+        }
 
-    public void Destroy()
-    {
-        if (container == IntPtr.Zero) { return; }
-        hl2da_api.IMT_Release(container);
-        Reset();
-    }
+        ~imt()
+        {
+            Dispose(false);
+        }
 
-    public static hl2da_imt Convert(IntPtr image, uint stride, uint height, hl2da_api.IMT_Format format_in, hl2da_api.IMT_Format format_out)
-    {
-        hl2da_imt fc = new hl2da_imt();
-        hl2da_api.IMT_YUV2RGB(image, stride, height, (uint)format_in, (uint)format_out, ref fc.container);
-        if (fc.container != IntPtr.Zero) { hl2da_api.IMT_Extract(fc.container, ref fc.buffer, ref fc.length); }
-        return fc;
-    }
+        public IntPtr Buffer { get { return buffer; } }
 
-    public static uint GetStride_PV(uint width)
-    {
-        return (width + 63U) & ~63U;
+        public int Length { get { return length; } }
+
+        private void Reset()
+        {
+            container = IntPtr.Zero;
+            buffer = IntPtr.Zero;
+            length = 0;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (container == IntPtr.Zero) { return; }
+            hl2da_api.IMT_Release(container);
+            Reset();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public static imt Convert(IntPtr image, uint stride, uint height, hl2da_api.IMT_Format format_in, hl2da_api.IMT_Format format_out)
+        {
+            imt fc = new imt();
+            hl2da_api.IMT_YUV2RGB(image, stride, height, (uint)format_in, (uint)format_out, ref fc.container);
+            if (fc.container != IntPtr.Zero) { hl2da_api.IMT_Extract(fc.container, ref fc.buffer, ref fc.length); }
+            return fc;
+        }
+
+        public static uint GetStride_PV(uint width)
+        {
+            return (width + 63U) & ~63U;
+        }
     }
 }
